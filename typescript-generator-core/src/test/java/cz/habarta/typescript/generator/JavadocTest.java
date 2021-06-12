12d0e0ce127beebc0dff7cafe86a759038de3fa0
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2021-06-12 09:34:48 +0300

    partially fixed JavadocTest.testJavadoc

--- typescript-generator-core/src/test/java/cz/habarta/typescript/generator/JavadocTest.java
+++ typescript-generator-core/src/test/java/cz/habarta/typescript/generator/JavadocTest.java
@@ -25,10 +25,10 @@ public class JavadocTest {
             final BeanModel bean = model.getBeans().get(0);
             Assert.assertEquals("Documentation for ClassWithJavadoc. First line.", bean.getComments().get(0));
             Assert.assertEquals("Second line.", bean.getComments().get(1));
-            final PropertyModel property1 = bean.getProperties().get(0);
-            Assert.assertEquals("Documentation for documentedField.", property1.getComments().get(0));
-            final PropertyModel property2 = bean.getProperties().get(1);
+            final PropertyModel property2 = bean.getProperties().get(0);
             Assert.assertEquals("Documentation for documentedEnumField.", property2.getComments().get(0));
+            final PropertyModel property1 = bean.getProperties().get(1);
+            Assert.assertEquals("Documentation for documentedField.", property1.getComments().get(0));
             final EnumModel enumModel = model.getEnums().get(0);
             Assert.assertEquals("Documentation for DummyEnum.", enumModel.getComments().get(0));
         }
